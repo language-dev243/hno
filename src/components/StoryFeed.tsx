fa134eb832c363b97c3580acd33100b11e4749f8
language-dev243 <language-dev@protonmail.com>
2025-03-25 09:24:01 +0100

    adding useState to track the count of how many stories are loaded

--- src/components/StoryFeed.tsx
+++ src/components/StoryFeed.tsx
@@ -1,9 +1,11 @@
-import StoryCard from './StoryCard';
-
+import { useState } from 'react';
 import { useTopStories } from '../hooks/fetchTopStories';
+
+import StoryCard from './StoryCard';
 
 const StoryFeed = () => {
   const { data, isLoading, error } = useTopStories();
+  const [ storiesLoaded, setStoriesLoaded ] = useState(10)
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error fetching data</div>;
@@ -13,7 +15,7 @@ const StoryFeed = () => {
   return (
     <div className="bg-gray-800 text-white p-4 flex justify-center items-center">
       <ul>
-        {data.slice(0, 10).map((id) => (
+        {data.slice(0, storiesLoaded).map((id) => (
           <StoryCard key={id} storyID={id} />
         ))}
       </ul>
