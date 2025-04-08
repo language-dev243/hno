db57ba838b81ca3ca75f3567ecf6eb1e03ad9739
language-dev243 <language-dev@protonmail.com>
2025-04-08 11:12:17 +0200

    installation, configuration of vitest and jsdom, setting up simple test cases to check the installation+config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
@@ -5,4 +7,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    environment: 'jsdom',
+    globals: true, // no need to import keywords such as describe, test and expect into the tests.
+  }
 });
